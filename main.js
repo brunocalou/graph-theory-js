9cbba213ed7608ea6c0fd8205b306af072989c4b
brunocalou <brunocaloualves@gmail.com>
2015-11-21 17:48:19 -0200

    Added load graph from file method and calculate degree statistics. Changed project structure. Implemented theese functions to the adjacency vector only

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-var graph = require('./graph');
+var graph = require('./src/graph/graph');
 
 //Temporary debug tests
 
-var my_graph = new graph.Graph(graph.DataStructure.ADJACENCY_LIST);
-my_graph.loadFromFile('my_graph.txt');
+var my_graph = new graph.Graph(graph.DataStructure.ADJACENCY_VECTOR);
+// my_graph.loadFromFile('./src/test/graph_files/as_graph.txt');
+my_graph.loadFromFile('./src/test/graph_files/small_graph.txt');
+my_graph.saveGraphStatisticsToFile();
 my_graph.BFS();
 my_graph.DFS();
 my_graph.findClusters();
