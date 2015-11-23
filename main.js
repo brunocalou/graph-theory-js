d1b696d50f855bd8695b319d790568f3e57db6da
brunocalou <brunocaloualves@gmail.com>
2015-11-22 23:15:20 -0200

    Changed project structure

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var graph = require('./src/graph/graph');
 
 var my_graph = new graph.Graph(graph.DataStructure.ADJACENCY_VECTOR);
 // my_graph.loadFromFile('./src/test/graph_files/as_graph.txt');
-my_graph.loadFromFile('./src/test/graph_files/small_graph.txt');
+my_graph.loadFromFile('./test/graph_files/small_graph.txt');
 my_graph.saveGraphStatisticsToFile();
 my_graph.BFS();
 my_graph.DFS();
