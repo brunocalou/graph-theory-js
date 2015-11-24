1284dd7c7b4d9af86b26bad4d8c8eb432521c752
brunocalou <brunocaloualves@gmail.com>
2015-11-24 17:47:48 -0200

    Changed timer library to get time in nano seconds

--- src/timer/timer.js
+++ src/timer/timer.js
@@ -1,17 +1,31 @@
 var Timer = function () {
-	this.initial_time = Date.now();
+	this.initial_time = process.hrtime();
 }
 
 Timer.prototype.start = function () {
-	this.initial_time = Date.now();
+	this.initial_time = process.hrtime();
 };
 
 Timer.prototype.getElapsedTime = function () {
-	return Date.now() - this.initial_time;
+	//Get the time in nano seconds
+	var diff = process.hrtime(this.initial_time);
+	return diff[0] * 1e9 + diff[1];
+};
+
+Timer.prototype.printElapsedTimeNano = function () {
+	console.log(this.getElapsedTime() + ' ns');
+};
+
+Timer.prototype.printElapsedTimeMicro = function () {
+	console.log(this.getElapsedTime() / 1000 + ' us');
+};
+
+Timer.prototype.printElapsedTimeMilli = function () {
+	console.log(this.getElapsedTime() / 1000000 + ' ms');
 };
 
 Timer.prototype.printElapsedTime = function () {
-	console.log(Date.now() - this.initial_time + ' ms');
+	//Print the time in seconds
+	console.log(this.getElapsedTime() / 1000000000 + ' s');
 };
-
 module.exports = Timer;
