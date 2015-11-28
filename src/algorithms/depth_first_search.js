1827ba972b84dfb7d2c663174b7518886728f06e
brunocalou <brunocaloualves@gmail.com>
2015-11-28 10:47:52 -0200

    Added degrees to the spanning tree. Formated the DFS code

--- src/algorithms/depth_first_search.js
+++ src/algorithms/depth_first_search.js
@@ -5,32 +5,37 @@ function DFS(graph, initial_vertex) {
 	var stack = new Stack();
 	var visited_vertices = new Array(graph.number_of_vertices);
 	var spanning_tree = new Array(graph.number_of_vertices);
-	var parent;
-	
+	var degrees = new Array(graph.number_of_vertices);
+
 	stack.push(initial_vertex);
 	spanning_tree[initial_vertex] = null; //Mark the root
-	
+	degrees[initial_vertex] = 0;
+
 	while (!stack.isEmpty()) {
 		var vertex = stack.pop();
 		console.log("Removed " + vertex + " from the stack");
-		
-		if(!visited_vertices[vertex]) {
+
+		if (!visited_vertices[vertex]) {
 			visited_vertices[vertex] = true;
-			
-			graph.forEachNeighbor(vertex, function(neighbor) {
+
+			graph.forEachNeighbor(vertex, function (neighbor) {
 				if (!visited_vertices[neighbor]) {
 					stack.push(neighbor);
 					console.log("Added " + neighbor);
 					
 					//Add the vertex as the parent of the neighbor on the spaning tree
 					spanning_tree[neighbor] = vertex;
+					//The degree of a neighbor is the degree of its parent +1
+					degrees[neighbor] = degrees[vertex] + 1;
 				}
 			});
 		}
 	}
 	console.log("Spanning tree = ");
 	console.log(spanning_tree);
-	return new SpanningTree(initial_vertex, spanning_tree);	
+	console.log("Degrees = ");
+	console.log(degrees);
+	return new SpanningTree(initial_vertex, spanning_tree, degrees);
 };
 
 module.exports = DFS;
--- src/trees/spanning_tree.js
+++ src/trees/spanning_tree.js
@@ -1,6 +1,7 @@
-var SpanningTree = function (root, tree) {
+var SpanningTree = function (root, tree, degrees) {
 	this.root = root;
 	this.tree = tree; //Array that holds the parent of each vertex (vertices are defined by the index on the array)
+	this.degrees = degrees; //Stores de degree of every vertex. The root has degree 0
 };
 
 module.exports = SpanningTree;
